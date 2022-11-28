ad8106a107a4217437421972b91a5b9dbf6c8850
kartikey <dwivedi.k@northeastern.edu>
2022-11-27 19:41:22 -0500

    update backend env file

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_URL
-console.log("API_BASE", API_BASE);
+const API_BASE = process.env.REACT_APP_API_URL
+console.log("API_BASE ", API_BASE);
 
 export const loginUser = async (user) => {
     const response = await axios.post(`${API_BASE}/api/login`, user)
--- microservices/backend/app.js
+++ microservices/backend/app.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import UsersController from './controllers/users/user-controller.js';
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.PROJECT_DB_CONNECTION_STRING
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/Codify';
+console.log("CONNECTION_STRING ", CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
-console.log(CONNECTION_STRING);
 
 const corsOptions = {
     origin: 'http://localhost:3000',
