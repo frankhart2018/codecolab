18dce2c7046d24608ddc9f43a5294909c6b9d0a8
frankhart2018 <choudhury.si@northeastern.edu>
2022-11-29 14:18:27 -0500

    Add cors middleware to pyrunner

--- microservices/pyrunner/app.py
+++ microservices/pyrunner/app.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import subprocess
 import os
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 tags_metadata = [
     {
