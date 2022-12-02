ba026abf8d2fb2a3d0de6fa4f3ffbffccf6746de
Kartikey Dwivedi <47808038+kartikeydwi1997@users.noreply.github.com>
2022-12-02 01:24:35 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { SnackbarProvider } from 'notistack';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
+    <SnackbarProvider maxSnack={3}>
     <App />
+    </SnackbarProvider>
   </Router>
 );
