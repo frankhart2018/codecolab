83e562e1da51f61a89136ab354cc0276f1050e49
frankhart2018 <choudhury.si@northeastern.edu>
2022-12-07 02:27:50 -0500

    Add top level new file and dir creation buttons

--- frontend/src/components/code-editor-screen/file-structure/FileStructure.js
+++ frontend/src/components/code-editor-screen/file-structure/FileStructure.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router";
+import * as Icon from "react-feather";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -10,10 +11,23 @@ import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import FolderIcon from "@mui/icons-material/Folder";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 import "./FileStructure.css";
-import { getProjectByIdThunk } from "../../../services/project-thunk";
+import {
+  createDirInProjectThunk,
+  createFileInProjectThunk,
+  getProjectByIdThunk,
+} from "../../../services/project-thunk";
 import { updateFileMap } from "../../../reducers/project-reducer";
 import FileStructureMenu from "./FileStructureMenu";
 
@@ -108,6 +122,10 @@ export default function FileStructure() {
   const { fileMap, fileMapLoading } = useSelector((state) => state.project);
   const [currentPath, setCurrentPath] = useState("");
   const [currentType, setCurrentType] = useState("");
+  const [openNewDirDialog, setOpenNewDirDialog] = useState(false);
+  const [newDirName, setNewDirName] = useState("");
+  const [openNewFileDialog, setOpenNewFileDialog] = useState(false);
+  const [newFileName, setNewFileName] = useState("");
 
   const { pathname } = useLocation();
   const path_split = pathname.split("/");
@@ -128,48 +146,153 @@ export default function FileStructure() {
     return currentType;
   };
 
-  return (
-    <Box sx={{ height: "100vh", width: "25%" }} className="background-grayish">
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-        }}
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-        className="background-grayish"
-        subheader={
-          <Typography
-            variant="body1"
-            align="center"
-            className="text-gray"
-            sx={{ fontWeight: "bold" }}
-          >
-            Project files
-          </Typography>
-        }
-      >
-        {fileMapLoading && "Loading..."}
-        {!fileMapLoading &&
-          fileMap !== null &&
-          fileMap.map((root, i) => {
-            let path = root.name;
-            return generateFileStructure(
-              root,
-              path,
-              dispatch,
-              i,
-              setCurrentPath,
-              setCurrentType
-            );
-          })}
-      </List>
+  const handleOpenNewDirDialogOpen = () => {
+    setOpenNewDirDialog(true);
+  };
 
-      <FileStructureMenu
-        projectId={project_id}
-        getCurrentPath={getCurrentPath}
-        getCurrentType={getCurrentType}
-      />
-    </Box>
+  const handleOpenNewFileDialogOpen = () => {
+    setOpenNewFileDialog(true);
+  };
+
+  const handleOpenNewDirDialogClose = () => {
+    setOpenNewDirDialog(false);
+    setNewDirName("");
+  };
+
+  const handleOpenNewFileDialogClose = () => {
+    setOpenNewFileDialog(false);
+    setNewFileName("");
+  };
+
+  const handleNewDirCreateBtnClicked = () => {
+    dispatch(
+      createDirInProjectThunk({
+        project_id: project_id,
+        path: currentPath,
+        dir_name: newDirName,
+      })
+    );
+    setOpenNewDirDialog(false);
+    setNewDirName("");
+  };
+
+  const handleNewFileCreateBtnClicked = () => {
+    dispatch(
+      createFileInProjectThunk({
+        project_id: project_id,
+        path: currentPath,
+        file_name: newFileName,
+      })
+    );
+    setOpenNewFileDialog(false);
+    setNewFileName("");
+  };
+
+  return (
+    <>
+      <Box
+        sx={{ height: "100vh", width: "25%" }}
+        className="background-grayish"
+      >
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+          }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          className="background-grayish"
+          subheader={
+            <Typography
+              variant="body1"
+              align="center"
+              className="text-gray"
+              sx={{ fontWeight: "bold" }}
+            >
+              Project files
+            </Typography>
+          }
+        >
+          <Button
+            variant="contained"
+            sx={{ width: "50%" }}
+            onClick={handleOpenNewFileDialogOpen}
+          >
+            <Icon.File size={20} className="menu__icon" />
+            New File
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ width: "50%" }}
+            onClick={handleOpenNewDirDialogOpen}
+          >
+            <Icon.Folder size={20} className="menu__icon" />
+            New Directory
+          </Button>
+          {fileMapLoading && "Loading..."}
+          {!fileMapLoading &&
+            fileMap !== null &&
+            fileMap.map((root, i) => {
+              let path = root.name;
+              return generateFileStructure(
+                root,
+                path,
+                dispatch,
+                i,
+                setCurrentPath,
+                setCurrentType
+              );
+            })}
+        </List>
+
+        <FileStructureMenu
+          projectId={project_id}
+          getCurrentPath={getCurrentPath}
+          getCurrentType={getCurrentType}
+        />
+      </Box>
+
+      <Dialog open={openNewFileDialog} onClose={handleOpenNewFileDialogClose}>
+        <DialogContent>
+          <DialogContentText>Add new file</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="filename"
+            label="File name"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={newFileName}
+            onChange={(e) => setNewFileName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleOpenNewFileDialogClose}>Cancel</Button>
+          <Button onClick={handleNewFileCreateBtnClicked}>Create</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openNewDirDialog} onClose={handleOpenNewDirDialogClose}>
+        <DialogContent>
+          <DialogContentText>Add new directory</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="dirname"
+            label="Directory name"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={newDirName}
+            onChange={(e) => setNewDirName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleOpenNewDirDialogClose}>Cancel</Button>
+          <Button onClick={handleNewDirCreateBtnClicked}>Create</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
