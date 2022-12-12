995c3a1e976279970961d70871af023813c55722
kartikey <dwivedi.k@northeastern.edu>
2022-12-12 02:42:52 -0500

    clean code

--- frontend/src/Home/components/StarredProjects.js
+++ frontend/src/Home/components/StarredProjects.js
@@ -8,9 +8,6 @@ import { styled } from "@mui/material/styles";
 const StyledBadge = styled(Badge)(({ theme }) => ({
     "& .MuiBadge-badge": {
         right: 4
-        // top: 13,
-        // border: `2px solid ${theme.palette.background.paper}`,
-        // padding: "0 4px"
     }
 }));
 
